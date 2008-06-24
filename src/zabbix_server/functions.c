b47e0c00950bf5125b4a889071a818c4368a4f14
Max Lapan <max.lapan@gmail.com>
2008-06-24 12:25:55 +0400

    [hfs] Trends storage hooks aded in functions.c

--- src/zabbix_server/functions.c
+++ src/zabbix_server/functions.c
@@ -472,18 +472,22 @@ static int	add_history(DB_ITEM *item, AGENT_RESULT *value, int now)
 				if(item->value_type==ITEM_VALUE_TYPE_UINT64)
 				{
 					if(GET_UI64_RESULT(value))
-						if (CONFIG_HFS_PATH)
+						if (CONFIG_HFS_PATH) {
 							HFSadd_history_uint (CONFIG_HFS_PATH, item->siteid, item->itemid, item->delay, value->ui64, now);
+							HFSadd_trend_uint (CONFIG_HFS_PATH, item->siteid, item->itemid, value->ui64, now);
+						}
 						else
 							DBadd_history_uint(item->itemid,value->ui64,now);
 				}
 				else if(item->value_type==ITEM_VALUE_TYPE_FLOAT)
 				{
-				    if(GET_DBL_RESULT(value))
-					if (CONFIG_HFS_PATH)
-						HFSadd_history (CONFIG_HFS_PATH, item->siteid, item->itemid, item->delay, value->dbl, now);
-					else
-						DBadd_history(item->itemid,value->dbl,now);
+					if(GET_DBL_RESULT(value))
+						if (CONFIG_HFS_PATH) {
+							HFSadd_history (CONFIG_HFS_PATH, item->siteid, item->itemid, item->delay, value->dbl, now);
+							HFSadd_trend (CONFIG_HFS_PATH, item->siteid, item->itemid, value->dbl, now);
+						}
+						else
+							DBadd_history(item->itemid,value->dbl,now);
 				}
 			}
 			/* Delta as speed of change */
@@ -494,23 +498,28 @@ static int	add_history(DB_ITEM *item, AGENT_RESULT *value, int now)
 				{
 					if(GET_DBL_RESULT(value) && (item->prevorgvalue_null == 0) && (item->prevorgvalue_dbl <= value->dbl) && (now != item->lastclock))
 					{
-						if (CONFIG_HFS_PATH)
+						if (CONFIG_HFS_PATH) {
 							HFSadd_history (CONFIG_HFS_PATH, item->siteid, item->itemid, item->delay, 
-								(value->dbl - item->prevorgvalue_dbl)/(now-item->lastclock), now);
+									(value->dbl - item->prevorgvalue_dbl)/(now-item->lastclock), now);
+							HFSadd_trend (CONFIG_HFS_PATH, item->siteid, item->itemid, 
+								      (value->dbl - item->prevorgvalue_dbl)/(now-item->lastclock), now);
+						}
 						else
-							DBadd_history(
-								item->itemid,
-								(value->dbl - item->prevorgvalue_dbl)/(now-item->lastclock),
-								now);
+							DBadd_history(item->itemid,
+								      (value->dbl - item->prevorgvalue_dbl)/(now-item->lastclock),
+								      now);
 					}
 				}
 				else if( ITEM_VALUE_TYPE_UINT64 == item->value_type )
 				{
 					if(GET_UI64_RESULT(value) && (item->prevorgvalue_null == 0) && (item->prevorgvalue_uint64 <= value->ui64) && (now != item->lastclock))
 					{
-						if (CONFIG_HFS_PATH)
+						if (CONFIG_HFS_PATH) {
 							HFSadd_history_uint (CONFIG_HFS_PATH, item->siteid, item->itemid, item->delay, 
 								(value->ui64 - item->prevorgvalue_uint64)/(now-item->lastclock), now);
+							HFSadd_trend_uint (CONFIG_HFS_PATH, item->siteid, item->itemid,
+									   (value->ui64 - item->prevorgvalue_uint64)/(now-item->lastclock), now);
+						}
 						else
 							DBadd_history_uint(
 								item->itemid,
@@ -527,8 +536,10 @@ static int	add_history(DB_ITEM *item, AGENT_RESULT *value, int now)
 				{
 					if(GET_DBL_RESULT(value) && (item->prevorgvalue_null == 0) && (item->prevorgvalue_dbl <= value->dbl) )
 					{
-						if (CONFIG_HFS_PATH)
+						if (CONFIG_HFS_PATH) {
 							HFSadd_history (CONFIG_HFS_PATH, item->siteid, item->itemid, item->delay, value->dbl-item->prevorgvalue_dbl, now);
+							HFSadd_trend (CONFIG_HFS_PATH, item->siteid, item->itemid, value->dbl-item->prevorgvalue_dbl, now);
+						}
 						else
 							DBadd_history(item->itemid, (value->dbl - item->prevorgvalue_dbl), now);
 					}
@@ -537,9 +548,12 @@ static int	add_history(DB_ITEM *item, AGENT_RESULT *value, int now)
 				{
 					if(GET_UI64_RESULT(value) && (item->prevorgvalue_null == 0) && (item->prevorgvalue_uint64 <= value->ui64) )
 					{
-						if (CONFIG_HFS_PATH)
+						if (CONFIG_HFS_PATH) {
 							HFSadd_history_uint (CONFIG_HFS_PATH, item->siteid, item->itemid, item->delay, 
 								value->ui64-item->prevorgvalue_uint64, now);
+							HFSadd_trend_uint (CONFIG_HFS_PATH, item->siteid, item->itemid,
+									   value->ui64-item->prevorgvalue_uint64, now);
+						}
 						else
 							DBadd_history_uint(item->itemid, value->ui64 - item->prevorgvalue_uint64, now);
 					}
