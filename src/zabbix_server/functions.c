82b3bd1beda8b1915f229d5aae261be60571b0ae
Max Lapan <max.lapan@gmail.com>
2008-08-04 00:52:37 +0400

    [hfs] Don't update item's status and triggers when we got history value.

--- src/zabbix_server/functions.c
+++ src/zabbix_server/functions.c
@@ -951,8 +951,10 @@ void	process_new_value(DB_ITEM *item, AGENT_RESULT *value, time_t timestamp)
 	}
 
 	add_history(item, value, now);
-	update_item(item, value, now);
-	update_functions( item );
+	if (timestamp) {
+		update_item(item, value, now);
+		update_functions( item );
+	}
 }
 
 /*
