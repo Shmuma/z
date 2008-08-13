67db79fd3486ed21e901c652644c6fa5f8184058
Max Lapan <max.lapan@gmail.com>
2008-08-13 17:32:49 +0400

    [php] Fix check for hfs availability

--- frontends/php/include/hfs.inc.php
+++ frontends/php/include/hfs.inc.php
@@ -2,6 +2,6 @@
 // check that HFS module is loaded
 function zbx_hfs_available ()
 {
-	return function_exists ("zabbix_hfs_host_availability");
+	return function_exists ("zabbix_hfs_read_history");
 }
 ?>
