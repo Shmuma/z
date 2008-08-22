cdacf09466a59c709ca01d8d6e40f183bf0efbb0
Max Lapan <max.lapan@gmail.com>
2008-08-22 13:11:14 +0400

    [pkg] Another naming scheme for freebsd packages

--- spec/freebsd/pkg.sh
+++ spec/freebsd/pkg.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 cp -f ../../misc/zabbix-rebase-server sbin/
-pkg_create -v -i inst -I pinst -f plist -s `pwd` -S `pwd` -p / -d descr -c comm zabbix_agent.tgz
+
+VER=1.4.4_18
+
+# replace version in @name tag
+cat plist | sed "s/^@name.*$/@name zabbix_agent_yandex-$VER/" > plist.tmp && mv plist.tmp plist
+pkg_create -v -i inst -I pinst -f plist -s `pwd` -S `pwd` -p / -d descr -c comm zabbix_agent_yandex-$VER.tgz
