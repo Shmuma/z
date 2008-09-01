d3dbae69a19060c4bd519a8d48bbd93f4ee8e5ee
Max Lapan <max.lapan@gmail.com>
2008-09-01 16:01:16 +0400

    [php,hfs] Update item's status when item disabled.

--- src/php5-zabbix/php_zabbix.h
+++ src/php5-zabbix/php_zabbix.h
@@ -24,6 +24,7 @@ PHP_FUNCTION(zabbix_hfs_read_str);
 PHP_FUNCTION(zabbix_hfs_last_str);
 PHP_FUNCTION(zabbix_hfs_hosts_availability);
 PHP_FUNCTION(zabbix_hfs_item_status);
+PHP_FUNCTION(zabbix_hfs_update_item_status);
 PHP_FUNCTION(zabbix_hfs_item_stderr);
 PHP_FUNCTION(zabbix_hfs_item_values);
 PHP_FUNCTION(zabbix_hfs_trigger_value);
