8eceea665593515cd4780fa107db2d6fbf86b8f2
Max Lapan <max.lapan@gmail.com>
2008-10-07 21:17:16 +0400

    [sms] Config fixes.

--- spec/build.sh
+++ spec/build.sh
@@ -18,6 +18,7 @@ ssh ztop1<<'EOF'
 cd rpm
 rpmbuild -ba */spec/zabbix-oracle.spec > log.txt 2>&1
 rpmbuild -ba */spec/zabbix-conf-zabbix.spec > log-conf-zabbix.txt 2>&1
+rpmbuild -ba */spec/zabbix-sms.spec > log-sms.txt 2>&1
 EOF
 
 for host in zab1 dfs2; do
