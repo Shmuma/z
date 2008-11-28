c079381a43c6070449d757658ac673cf489a078a
Max Lapan <max.lapan@gmail.com>
2008-11-28 18:37:43 +0300

    [pkg] New release.

--- spec/freebsd/pkg.sh
+++ spec/freebsd/pkg.sh
@@ -4,7 +4,7 @@ cp -f ../../misc/zabbix-rebase-server sbin/
 cp -f ../../misc/checks/bin/* etc/zabbix/bin/
 cp -f ../../misc/checks/conf.d/* etc/zabbix/conf.d/
 
-VER=1.4.4_18
+VER=1.4.4_43
 
 # replace version in @name tag
 cat plist | sed "s/^@name.*$/@name zabbix_agent_yandex-$VER/" > plist.tmp && mv plist.tmp plist
