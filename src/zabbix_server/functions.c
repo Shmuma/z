bb1aa207724892ad8eb5fc4d344d20fc14aa0fe5
Max Lapan <max.lapan@gmail.com>
2008-12-15 10:20:09 +0300

    [srv] Fucking distinct, part2

--- src/zabbix_server/functions.c
+++ src/zabbix_server/functions.c
@@ -137,7 +137,7 @@ void	update_triggers(zbx_uint64_t itemid)
 	zabbix_log( LOG_LEVEL_DEBUG, "In update_triggers [itemid:" ZBX_FS_UI64 "]",
 		itemid);
 
-	result = DBselect("select distinct t.triggerid,t.expression,t.description,t.url,t.comments,t.status,t.value,t.priority from triggers t,functions f,items i where i.status<>%d and i.itemid=f.itemid and t.status=%d and f.triggerid=t.triggerid and f.itemid=" ZBX_FS_UI64,
+	result = DBselect("select t.triggerid,t.expression,t.description,t.url,t.comments,t.status,t.value,t.priority from triggers t,functions f,items i where i.status<>%d and i.itemid=f.itemid and t.status=%d and f.triggerid=t.triggerid and f.itemid=" ZBX_FS_UI64,
 		ITEM_STATUS_NOTSUPPORTED,
 		TRIGGER_STATUS_ENABLED,
 		itemid);
