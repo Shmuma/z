61a4641a24dc41c89578979a1cf509c65e62ed32
Max Lapan <max.lapan@gmail.com>
2008-12-23 15:22:03 +0300

    [pair] Fixed prioritization bug.

--- misc/pairs/alerter.d/media_sms.sh
+++ misc/pairs/alerter.d/media_sms.sh
@@ -5,7 +5,12 @@
 PRIO=5
 
 if [ ! -z $ZABBIX_ALERT_ID ]; then
-    PRIO=`mysql zabbix --connect_timeout=1 -e "select t.priority from triggers t, alerts a where t.triggerid=a.triggerid and a.alertid=$ZABBIX_ALERT_ID" -B -N`
+    PRIO=`mysql zabbix --connect_timeout=1 -u root -e "select t.priority from triggers t, alerts a where t.triggerid=a.triggerid and a.alertid=$ZABBIX_ALERT_ID" -B -N`
+
+    echo $PRIO >> /tmp/prio.txt
+    if [ a$PRIO = a ]; then
+	PRIO=5
+    fi
 
     if [ ! \( $PRIO -ge 0 -a $PRIO -le 5 \) ]; then
         PRIO=5
