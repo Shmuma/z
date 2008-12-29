6b704b08ed1cb99de9e5a0f924b27a537a2bec08
Max Lapan <max.lapan@gmail.com>
2008-12-29 18:01:18 +0300

    [hfs] hfs_func_upd finished.

--- src/utils/hfs_func_upd.c
+++ src/utils/hfs_func_upd.c
@@ -51,6 +51,13 @@ void dbitem_serialize(DB_ITEM *item, size_t item_len)
 
 int main (int argc, char** argv)
 {
+	DB_RESULT	result;
+	DB_ROW		row;
+	zbx_uint64_t	id;
+	char*		value;
+	hfs_function_value_t fun_val;
+	int		index = 0;
+
 	static struct cfg_line cfg[] = {
 		{"DBHost",&CONFIG_DBHOST,0,TYPE_STRING,PARM_OPT,0,0},
 		{"DBName",&CONFIG_DBNAME,0,TYPE_STRING,PARM_MAND,0,0},
@@ -58,6 +65,7 @@ int main (int argc, char** argv)
 		{"DBPassword",&CONFIG_DBPASSWORD,0,TYPE_STRING,PARM_OPT,0,0},
 		{"DBSocket",&CONFIG_DBSOCKET,0,TYPE_STRING,PARM_OPT,0,0},
 		{"DBPort",&CONFIG_DBPORT,0,TYPE_INT,PARM_OPT,1024,65535},
+		{"ServerHistoryFSPath",&CONFIG_HFS_PATH,0,TYPE_STRING,PARM_OPT,0,0},
 		{"ServerSite",&CONFIG_SERVER_SITE,0,TYPE_STRING,PARM_OPT,0,0},	
 		{0}
 	};
@@ -68,7 +76,22 @@ int main (int argc, char** argv)
 	/* connect to database */
 	DBconnect (ZBX_DB_CONNECT_EXIT);
 	/* select all lastvalues from functions */
+	result = DBselect ("select functionid, lastvalue from functions where lastvalue != ''");
+
 	/* update them in HFS */
+	while (row = DBfetch (result)) {
+		ZBX_STR2UINT64 (id, row[0]);
+		value = row[1];
+
+		if (HFS_convert_function_str2val (value, &fun_val))
+			if (HFS_save_function_value (CONFIG_HFS_PATH, CONFIG_SERVER_SITE, id, &fun_val))
+				printf ("Converted item %d of %lld. ID=%lld, val='%s', type=%d\n", index++, result->row_count, id, value, fun_val.type);
+			else
+				printf ("Item %d save failed. ID=%lld, val=%s\n", index++, id, value);
+		else
+			printf ("Item %d convert failed. ID=%lld, val=%s\n", index++, id, value);
+	}
+
 	DBclose ();
 }
 
