d107c919dc68cd3dc3105ae85ab34a506394b55d
Max Lapan <max.lapan@gmail.com>
2008-12-29 16:32:08 +0300

    [hfs] hfs_func_update in progress.

--- src/utils/hfs_func_upd.c
+++ src/utils/hfs_func_upd.c
@@ -1,4 +1,5 @@
 #include "config.h"
+#include "cfg.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,10 +18,33 @@ char usage_message[] = "Usage";
 char *help_message[] = { "Help", 0 };
 
 
+char	*CONFIG_DBHOST			= NULL;
+char	*CONFIG_DBNAME			= NULL;
+char	*CONFIG_DBUSER			= NULL;
+char	*CONFIG_DBPASSWORD		= NULL;
+char	*CONFIG_DBSOCKET		= NULL;
+int	CONFIG_DBPORT			= 0;
+
+char	*CONFIG_HFS_PATH		= NULL;
+char	*CONFIG_SERVER_SITE		= NULL;
+
 
 int main (int argc, char** argv)
 {
+	static struct cfg_line cfg[] = {
+		{"DBHost",&CONFIG_DBHOST,0,TYPE_STRING,PARM_OPT,0,0},
+		{"DBName",&CONFIG_DBNAME,0,TYPE_STRING,PARM_MAND,0,0},
+		{"DBUser",&CONFIG_DBUSER,0,TYPE_STRING,PARM_OPT,0,0},
+		{"DBPassword",&CONFIG_DBPASSWORD,0,TYPE_STRING,PARM_OPT,0,0},
+		{"DBSocket",&CONFIG_DBSOCKET,0,TYPE_STRING,PARM_OPT,0,0},
+		{"DBPort",&CONFIG_DBPORT,0,TYPE_INT,PARM_OPT,1024,65535},
+		{"ServerSite",&CONFIG_SERVER_SITE,0,TYPE_STRING,PARM_OPT,0,0},	
+	};
+
+	parse_cfg_file ("/etc/zabbix/zabbix_server.conf", cfg);
+
 	/* read config file */
+
 	/* connect to database */
 	/* select all lastvalues from functions */
 	/* update them in HFS */
