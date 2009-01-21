8e286272241fe853c95f82048fb532df496c8b8a
Max Lapan <max.lapan@gmail.com>
2009-01-21 19:31:12 +0300

    [media] Production golem API script.

--- misc/pairs/alerter.d/media_golem.sh
+++ misc/pairs/alerter.d/media_golem.sh
@@ -3,11 +3,11 @@
 #MHOST=$(host -t PTR `cat /etc/zabbix/zabbix_server.conf | grep ListenIP | cut -d = -f 2` | cut -d ' ' -f 5 | sed 's/\.$//')
 OUT=/tmp/golem-reqs-$$.txt
 
-exec 2>> /tmp/golem-errs.txt
+exec 2> /tmp/golem-errs.txt
 
+#set -x
 #ZABBIX_ALERT_ID=83077
 
-SQL="select u.alias as User, a.clock as Clock, t.value as Value, t.description as Name, a.message as Description, if (t.priority > 2,'critical','warning') as Severity, h.host as Host from alerts a, triggers t, users u, functions f,items i, hosts h where a.alertid=$ZABBIX_ALERT_ID and a.triggerid=t.triggerid and a.userid=u.userid and f.triggerid=t.triggerid and f.itemid=i.itemid and h.hostid=i.hostid limit 1\G";
 SQL="select u.alias as User, a.clock as Clock, t.value as Value, t.description as Name, a.message as Description, if (t.priority > 2,'critical','warning') as Severity, h.host as Host from alerts a, triggers t, users u, functions f,items i, hosts h where a.alertid=$ZABBIX_ALERT_ID and a.triggerid=t.triggerid and a.userid=u.userid and f.triggerid=t.triggerid and f.itemid=i.itemid and h.hostid=i.hostid limit 1\G";
 
 #echo "" >> $OUT
@@ -17,7 +17,7 @@ mysql zabbix -B --user=root -e "$SQL" | sed 's/^ *//g' | tail -n +2 > $OUT
 
 host=$(cat $OUT | grep Host | cut -f 2 -d ' ')
 event=$(cat $OUT | grep Name | cut -f 2- -d ' ')
-descr=$(cat $OUT | grep Description | cut -f 2- -d ' ')
+descr=$(cat $OUT | grep Description | cut -f 2- -d ' ' | sed 's/&/%26;/g')
 value=$(cat $OUT | grep Value | cut -f 2 -d ' ')
 
 #echo $host $event $value $descr
@@ -28,6 +28,8 @@ if [ $value = 0 ]; then
 	extra="&status=ok"
 fi
 
-wget -q -O /dev/null "http://kohts.golem-test.yandex.net/api/event.sbml?object=$host&eventtype=$event&info=$descr$extra"
+#wget --no-check-certificate -q -O /dev/null "https://golem.yandex.net/api/event.sbml?object=$host&eventtype=$event&info=$descr$extra"
+wget --no-check-certificate -q -O /dev/null "https://golem.yandex.net/api/event.sbml?object=$host&eventtype=$event&info=$descr$extra"
+
 
 rm -f $OUT
