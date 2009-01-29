ece51ba2a23fab67dfa5ad960718b21c94886c5e
Max Lapan <max.lapan@gmail.com>
2009-01-29 16:16:16 +0300

    [pkg] FreeBSD

--- spec/freebsd/pkg.sh
+++ spec/freebsd/pkg.sh
@@ -8,4 +8,6 @@ VER=1.4.4_52
 
 # replace version in @name tag
 cat plist | sed "s/^@name.*$/@name zabbix_agent_yandex-$VER/" > plist.tmp && mv plist.tmp plist
+find etc usr -type f >> plist
+
 pkg_create -v -i inst -I pinst -f plist -s `pwd` -S `pwd` -p / -d descr -c comm zabbix_agent_yandex-$VER.tgz
