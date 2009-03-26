5742cdf1e01046f2f87b9ec6d998318969fb5535
Max Lapan <max.lapan@gmail.com>
2009-03-26 11:06:41 +0300

    [checks] mtail.sh under FreeBSD

--- misc/checks/bin/mtail.sh
+++ misc/checks/bin/mtail.sh
@@ -7,14 +7,28 @@
 
 STATE_PATH=/tmp/zabbix/mtail/
 
+case `uname` in
+    Linux)
+        MD5=md5sum
+        STAT="stat -c %s"
+        ;;
+    FreeBSD)
+        MD5=md5
+        STAT="stat -f %z"
+        ;;
+    *)
+        echo "Unknown OS -> unsupported" 1>&2
+        exit 0
+esac
+
 FILE=$1
-ID=`echo $FILE $2 | md5sum | cut -d ' ' -f 1`
+ID=`echo $FILE $2 | $MD5 | cut -d ' ' -f 1`
 OFS=0
 
 [ ! -d $STATE_PATH ] && mkdir -p $STATE_PATH
 [ -f $STATE_PATH/$ID ] && OFS=`cat $STATE_PATH/$ID`
 
-NEW_OFS=$((`stat -c %s $FILE`+1))
+NEW_OFS=$((`$STAT $FILE`+1))
 
 # reset offset if file becomes smaller (rotated?)
 [ $NEW_OFS -lt $OFS ] && OFS=0
