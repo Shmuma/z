1867fdc2b57a39f30ca38a9b8e4b78c6b0e83d85
Max Lapan <max.lapan@gmail.com>
2009-04-23 13:37:58 +0400

    [pkg] New release.

--- spec/freebsd/pkg.sh
+++ spec/freebsd/pkg.sh
@@ -4,7 +4,7 @@ cp -f ../../misc/zabbix-rebase-server usr/local/sbin/
 cp -f ../../misc/checks/bin/* usr/local/etc/zabbix/bin/
 cp -f ../../misc/checks/conf.d/* usr/local/etc/zabbix/conf.d/
 
-VER=1.4.4_59
+VER=1.4.4_60
 
 # replace version in @name tag
 cat > plist <<EOF
